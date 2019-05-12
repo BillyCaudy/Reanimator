08b0c3b8cace68d1a5521bd30e70acfdc9ed4c04
Lance Smith <lance.matthew.smith@gmail.com>
2019-05-12 15:28:53 -0700

    Add route for users to sign in

--- routes/api/users.js
+++ routes/api/users.js
@@ -34,4 +34,26 @@ router.post('/signup', (req, res) => {
   });
 });
 
+//sign in route for users
+router.post('/signin', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({ email: "This user doesn't exist" });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: "Success" });
+          } else {
+            return res.status(400).json({ password: "Incorrect password" });
+          }
+        });
+    });
+});
+
 module.exports = router;
