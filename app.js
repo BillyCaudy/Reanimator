8938c03273ea2aed97556b24cefd60d699ddd4cd
Lance Smith <lance.matthew.smith@gmail.com>
2019-05-14 16:52:38 -0700

    Change for Heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const friendships = require("./routes/api/friendships");
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
