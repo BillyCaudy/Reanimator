a2ebe1316857f3faf86f468a8845850d61bd7a7c
Lance Smith <lance.matthew.smith@gmail.com>
2019-05-15 10:00:18 -0700

    Try https for iframe

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -9,21 +9,21 @@ import ProfilePageContainer from './profile/profile_page_container';
 // import Iframe from 'react-iframe'
 
 const App = () => (
-    <div id="the-whole-enchilada">
-        <div id="left-navbar-wrapper">
-            <NavBarContainer/>
-            <Switch id="signin-switch">
-                {/* <AuthRoute exact path = "/" component = {HomePage}/> */}
-                <AuthRoute exact path="/login" component={LoginFormContainer} />
-                <AuthRoute exact path="/signup" component={SignupFormContainer} />
-                <ProtectedRoute exact path="/collections" component={CollectionIndexContainer} />
-                <ProtectedRoute exact path="/profile-page" component={ProfilePageContainer} />
-            </Switch>
-        </div>
-        <div id="main-content-wrapper">
-            <iframe title="unique" src="http://localhost:5000/stars.html" height="100%" width="100%"></iframe>
-        </div>
+  <div id="the-whole-enchilada">
+    <div id="left-navbar-wrapper">
+      <NavBarContainer />
+      <Switch id="signin-switch">
+        {/* <AuthRoute exact path = "/" component = {HomePage}/> */}
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <ProtectedRoute exact path="/collections" component={CollectionIndexContainer} />
+        <ProtectedRoute exact path="/profile-page" component={ProfilePageContainer} />
+      </Switch>
     </div>
+    <div id="main-content-wrapper">
+      <iframe title="unique" src="https://localhost:5000/stars.html" height="100%" width="100%"></iframe>
+    </div>
+  </div>
 )
 
 export default App;
