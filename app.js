874e60eb13c9d71c084ad60d8fed130b1036b545
Billy Caudy <williamjohncaudy@gmail.com>
2019-05-14 22:13:25 -0700

    Delete undesired style

--- app.js
+++ app.js
@@ -48,4 +48,6 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
+app.use(express.static('./sample-vids'));
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
